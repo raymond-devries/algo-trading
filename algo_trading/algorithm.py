7a04bf8a1a6d71b9e5a5772cf042e81e63d03a96
raymond-devries <raymond.l.devries@gmail.com>
2020-07-05 14:24:23 -0700

    Added attributes and basic methods for algorithm base class.

--- algo_trading/algorithm.py
+++ algo_trading/algorithm.py
@@ -1,10 +1,25 @@
-import abc
+from abc import ABC, abstractmethod
+
+import pandas as pd
 
 
-class Algorithm(abc.ABC):
-    @abc.abstractmethod
-    def __init__(self):
+class Algorithm(ABC):
+    def __init__(self, capital: float = 10000):
+        self.buying_power = capital
+        self.buys = pd.Series()
+        self.sells = pd.Series()
+
+    @abstractmethod
+    def setup(self):
         pass
 
+    @abstractmethod
     def algorithm(self):
         pass
+
+    def buy(self):
+        pass
+
+    def sell(self):
+        pass
+
--- algo_trading/tests/test_algorithm.py
+++ algo_trading/tests/test_algorithm.py
@@ -1,7 +1,35 @@
 import pytest
-from algo_trading import algorithm
+import pandas as pd
+import numbers
+
+from algo_trading.algorithm import Algorithm
+
+
+@pytest.fixture
+def create_algorithm_class():
+    class TestingAlgorithm(Algorithm):
+        def setup(self):
+            pass
+
+        def algorithm(self):
+            pass
+
+    testing_algorithm = TestingAlgorithm()
+    return testing_algorithm
 
 
 def test_algorithm_instantiation():
     with pytest.raises(TypeError):
-        algorithm.Algorithm()
+        Algorithm()
+
+
+attributes = [
+    ('buying_power', numbers.Real),
+    ('buys', pd.Series),
+    ('sells', pd.Series)
+]
+
+
+@pytest.mark.parametrize('attribute, expected_type', attributes)
+def test_algorithm_attribute_types(attribute, expected_type, create_algorithm_class):
+    assert isinstance(getattr(create_algorithm_class, attribute), expected_type)

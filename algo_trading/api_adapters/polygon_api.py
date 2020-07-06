894cfa082715cd309d0b19f65ccb3419edfeb815
raymond-devries <raymond.l.devries@gmail.com>
2020-07-06 00:06:35 -0700

    Added polygon api adapter.

---
+++ algo_trading/api_adapters/polygon_api.py
@@ -0,0 +1,3 @@
+from decouple import config
+
+POLYGON_API_KEY = config('POLYGON_API_KEY')
---
+++ algo_trading/tests/api_adapters/test_polygon_api.py
@@ -0,0 +1,6 @@
+from algo_trading.api_adapters.polygon_api import POLYGON_API_KEY
+
+
+def test_polygon_api_retrieval():
+    assert POLYGON_API_KEY == 'YourKeyHere'
+

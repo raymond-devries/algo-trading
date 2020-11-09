f0f50d94c11a121f4a3c1fdbaad345ce72a9d2b7
raymond-devries <raymond.l.devries@gmail.com>
2020-11-08 22:16:27 -0800

    Create data flow generator for backtesting.

--- algo_trading/data_flow/data_flow.py
+++ algo_trading/data_flow/data_flow.py
@@ -1,8 +1,42 @@
 import pandas as pd
+from algo_trading.api_adapters import polygon_api
+from abc import ABC, abstractmethod
 
 
-class DataFlow:
-    def __init__(self):
-        self.candles_df = pd.DataFrame(
-            columns=["open", "close", "high", "low", "volume"]
-        )
+class DataFlow(ABC):
+    def __init__(self, previous_points_needed: int):
+        self.candles_df = self.get_dataframe()
+        self.previous_points_needed = previous_points_needed
+
+    @abstractmethod
+    def get_dataframe(self) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def get_data_generator(self) -> pd.DataFrame:
+        pass
+
+
+class PolygonBackTestDataFlow(DataFlow):
+    def __init__(self, ticker: str, multiplier: int, timespan: str, from_: str, to: str, previous_points_needed: int):
+        self.ticker = ticker
+        self.multiplier = multiplier
+        self.timespan = timespan
+        self.from_ = from_
+        self.to = to
+        super().__init__(previous_points_needed)
+
+    def get_dataframe(self) -> pd.DataFrame:
+        return polygon_api.get_ticker_info(self.ticker, self.multiplier, self.timespan, self.from_, self.to)
+
+    def get_data_generator(self) -> pd.DataFrame:
+        for i in range(self.previous_points_needed, len(self.candles_df) + 1):
+            yield self.candles_df[:i]
+
+
+class PolygonLiveTradingDataFlow(DataFlow):
+    def get_dataframe(self, **kwargs):
+        pass
+
+    def get_data_generator(self):
+        pass
--- algo_trading/tests/data_flow/test_data_flow.py
+++ algo_trading/tests/data_flow/test_data_flow.py
@@ -1,10 +1,31 @@
 import pandas as pd
-
+import pytest
+from random import randint, seed, uniform
 from algo_trading.data_flow import data_flow
 
 
-def test_data_flow_init():
-    test_data_flow = data_flow.DataFlow()
-    assert list(test_data_flow.candles_df.columns) == list(
-        pd.DataFrame(columns=["open", "close", "high", "low", "volume"]).columns
-    )
+def get_fake_df(*args):
+    df = pd.DataFrame(columns=["volume", "open", "close", "high", "low"])
+    seed(35)
+    length = 20
+    df["volume"] = [randint(1000, 100000) for _ in range(length)]
+    df["open"] = [uniform(10, 20) for _ in range(length)]
+    df["close"] = [uniform(10, 20) for _ in range(length)]
+    df["high"] = [uniform(10, 20) for _ in range(length)]
+    df["low"] = [uniform(10, 20) for _ in range(length)]
+    return df
+
+
+@pytest.fixture
+def patch_get_dataframe(monkeypatch):
+    monkeypatch.setattr(data_flow.PolygonBackTestDataFlow, "get_dataframe", get_fake_df)
+
+
+def test_polygon_backtest_get_next(patch_get_dataframe):
+    instance = data_flow.PolygonBackTestDataFlow("TICKER", 1, "minute", "2020-1-1", "2020-2-1", 6)
+    df = instance.get_dataframe()
+    generator = instance.get_data_generator()
+    pd.testing.assert_frame_equal(next(generator), df[:6])
+    pd.testing.assert_frame_equal(next(generator), df[:7])
+    pd.testing.assert_frame_equal(list(generator)[-1], df)
+
